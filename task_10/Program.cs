4d93d3832fc57b3d3c505dbf49f26656c1104c43
LarisaZaripova <larik7lolik@gmail.com>
2022-02-09 10:20:56 +0300

    added task 10

---
+++ task_10/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 10: Показать вторую цифру трёхзначного числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine();
+int result = (num / 10) % 10;
+Console.WriteLine(result);
+
+
