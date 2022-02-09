d1eb3a46195e603d366b95d11e647984bbce68f5
LarisaZaripova <larik7lolik@gmail.com>
2022-02-09 10:59:29 +0300

    added task 12

---
+++ task_12/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 12: Удалить вторую цифру трёхзначного числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 1000 || num < 99)
+{
+    Console.WriteLine("Неравильное число");
+    return;
+}
+ int delete = 2;
+ var str = num.ToString();
+ num = int.Parse(str.Remove(str.Length - delete, 1));
+ Console.WriteLine(num);
