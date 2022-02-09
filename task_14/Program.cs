43e411a5e22559455db4289172125cae5946073a
LarisaZaripova <larik7lolik@gmail.com>
2022-02-09 11:26:14 +0300

    added task 14

---
+++ task_14/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 14: Найти третью цифру числа или сообщить, что её нет.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if(num <= 99)
+{
+    Console.WriteLine("Нет третьей цифры");
+    return;
+}
+int result = int.Parse(Convert.ToString(num)[2].ToString());
+if(result == 0)
+{ 
+    Console.WriteLine("Цифра равна 0 ");
+    return;
+}
+Console.WriteLine(result);
